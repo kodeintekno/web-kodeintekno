5e3a78c00e603f444d79ad25f4ffc2e073e777dd
Fadhil Akbar Djunaedi <fadhilakbar57894@gmail.com>
2025-01-03 13:04:36 +0700

    menghapus p

--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -4,7 +4,6 @@ const ContactUs = () => {
       <h1 className="text-4xl md:text-5xl lg:text-[65px] font-bold text-center my-7 md:my-7 lg:mt-[115px] lg:mb-[80px] text-[#FFB158]">
         Hubungi Profesional
       </h1>
-      <p>tekno</p>
     </>
   );
 };

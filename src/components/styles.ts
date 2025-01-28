04a192910de5e3a179b6ac5788798912c7eb98ff
kartonobinsaleh <kartono.saleh@gmail.com>
2025-01-28 17:54:33 +0700

    styling text

---
+++ src/components/styles.ts
@@ -0,0 +1,8 @@
+export const styles = {
+  heroHeadText:
+    "text-4xl md:text-6xl lg:text-7xl font-bold bg-gradient-to-r from-[#ffb158] to-[#f59e0b] text-transparent bg-clip-text leading-tight pb-5",
+  heroSubText1:
+    "font-medium text-white lg:text-[50px] sm:text-[40px] xs:text-[30px] text-[20px] lg:leading-[60px]",
+  heroSubText2:
+    "font-medium lg:text-[30px] sm:text-[25px] xs:text-[20px] text-[14px] lg:leading-[32px]",
+};

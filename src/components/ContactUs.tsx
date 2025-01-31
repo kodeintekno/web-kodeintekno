bba7f67de72096f930f3494bb60e9623980d51f4
kodeintekno <kodeintekno@gmail.com>
2025-02-01 06:46:40 +0700

    Add email service configuration to contact form functionality

--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -1,8 +1,79 @@
+"use client";
+
 import { contactImg } from "@/assets";
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 import Image from "next/image";
 import { styles } from "./styles";
 
+const serviceId = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID;
+const templateId = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID;
+const publicKey = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY;
+
 const ContactUs = () => {
+  const formRef = useRef<HTMLFormElement>(null);
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    const { target } = e;
+    const { name, value } = target;
+
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+
+    if (!serviceId || !templateId || !publicKey) {
+      alert("Konfigurasi layanan email tidak lengkap.");
+      setLoading(false);
+      return;
+    }
+
+    emailjs
+      .send(
+        serviceId,
+        templateId,
+        {
+          from_name: form.name,
+          to_name: "Kartono",
+          from_email: form.email,
+          to_email: "kartono.saleh@gmail.com",
+          message: form.message,
+        },
+        publicKey,
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert("Terima kasih. Kami akan segera menghubungi Anda.");
+
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+          console.error(error);
+
+          alert("Ups, terjadi kesalahan. Silakan coba lagi.");
+        },
+      );
+  };
+
   return (
     <div className="container mx-auto px-4 py-24" id="contact">
       <h1 className={`${styles.heroHeadText} text-center`}>
@@ -10,7 +81,11 @@ const ContactUs = () => {
       </h1>
       <div className="flex flex-col-reverse lg:flex-row justify-center items-center gap-10 lg:gap-20 h-auto lg:h-[80vh]">
         {/* Form */}
-        <form className="w-full max-w-lg p-4 space-y-6">
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className="w-full max-w-lg p-4 space-y-6"
+        >
           {/* Nama Lengkap */}
           <div>
             <label
@@ -23,6 +98,9 @@ const ContactUs = () => {
               type="text"
               id="name"
               name="name"
+              required
+              value={form.name}
+              onChange={handleChange}
               className="w-full border border-gray-300 rounded-md p-2 text-sm md:text-base lg:text-lg placeholder:text-sm md:placeholder:text-base lg:placeholder:text-lg focus:outline-none focus:ring-2 focus:ring-orange-400"
               placeholder="Nama Lengkap Anda"
             />
@@ -40,6 +118,9 @@ const ContactUs = () => {
               type="email"
               id="email"
               name="email"
+              required
+              value={form.email}
+              onChange={handleChange}
               className="w-full border border-gray-300 rounded-md p-2 text-sm md:text-base lg:text-lg placeholder:text-sm md:placeholder:text-base lg:placeholder:text-lg focus:outline-none focus:ring-2 focus:ring-orange-400"
               placeholder="Alamat Email Anda"
             />
@@ -48,14 +129,17 @@ const ContactUs = () => {
           {/* Jelaskan Kebutuhan */}
           <div>
             <label
-              htmlFor="description"
+              htmlFor="message"
               className="block text-sm  text-gray-700 mb-2 md:text-base lg:text-lg max-w-full md:max-w-[80%] lg:max-w-[90%] mx-auto lg:mx-0 leading-relaxed lg:leading-9"
             >
               Jelaskan kebutuhan anda
             </label>
             <textarea
-              id="description"
-              name="description"
+              id="message"
+              name="message"
+              required
+              value={form.message}
+              onChange={handleChange}
               className="w-full h-[150px] border border-gray-300 rounded-md p-2 text-sm md:text-base lg:text-lg placeholder:text-sm md:placeholder:text-base lg:placeholder:text-lg focus:outline-none focus:ring-2 focus:ring-orange-400"
               placeholder="Tulis kebutuhan Anda di sini"
             />
@@ -66,7 +150,7 @@ const ContactUs = () => {
             type="submit"
             className="w-full lg:w-auto px-8 py-2 text-white font-medium text-lg rounded-full bg-gradient-to-r from-[#ffb158] to-[#f59e0b] hover:from-[#f59e0b] hover:to-[#feab1b] transition"
           >
-            Mulai Konsultasi
+            {loading ? "Mengirim Pesan..." : "Kirim Pesan"}
           </button>
         </form>
 

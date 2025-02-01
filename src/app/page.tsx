42314056d0cd3e82410eefcdf569b11c62d300b1
kodeintekno <kodeintekno@gmail.com>
2025-02-01 17:09:08 +0700

    add WhatsappButton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import About from "@/components/About";
+import About from "@/components/ui/About";
 import ContactUs from "@/components/ContactUs";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import Service from "@/components/Service";
+import WhatsappButton from "@/components/ui/WhatsappButton";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       <Service />
       <ContactUs />
       <Footer />
+      <WhatsappButton />
     </>
   );
 }

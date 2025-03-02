23e3aadf2a4a724e029587a3eef0af1c087c9318
Fadhil Akbar Djunaedi <fadhilakbar57894@gmail.com>
2025-03-02 09:58:13 +0700

    chore : add Team Screen and Portofolio Screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,10 @@ import ContactUs from "@/components/ContactUs";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
+import PortofolioSection from "@/components/PortofolioSection";
 import Service from "@/components/Service";
+import Team from "@/components/Team";
+// import Team from "@/components/Team";
 import WhatsappButton from "@/components/ui/WhatsappButton";
 
 export default function Home() {
@@ -13,6 +16,8 @@ export default function Home() {
       <Hero />
       <About />
       <Service />
+      <Team />
+      <PortofolioSection />
       <ContactUs />
       <Footer />
       <WhatsappButton />
